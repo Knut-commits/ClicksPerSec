b5fd5e4224c17682b718dd5f21192c35c628e63f
Knut-commits <26knuto@stu.tes.tp.edu.tw>
2024-11-23 13:03:43 +0800

    added handle click method and paint componnet

--- GamePanel.java
+++ GamePanel.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
+
 public class GamePanel extends JPanel implements ActionListener {
      
 	private static final int panel_width= 600; 
@@ -25,7 +26,7 @@ public class GamePanel extends JPanel implements ActionListener {
 	private JButton clickButton; // java swing
 	private BufferedImage background;
 	private int backgroundX;
-	
+	private double distance;
 	
 	public GamePanel() {
 	    this.setPreferredSize(new Dimension(panel_width, panel_height)); // this method is inheirted from Jpanel
@@ -35,24 +36,52 @@ public class GamePanel extends JPanel implements ActionListener {
 	    player = new Player(10,150,10);// sets its starting position and how many pixels it moves per click
 	    timeLeft = Game_time;
 	    
-	    background = Image.IO.read(new File("background.jpg")); // will load a chosen image as the background
-	    
+	    background = ImageIO.read(new File("background.jpg")); // will load a chosen image as the background
+	    backgroundX = 0;
 	    
 	    timerLabel =  new JLabel("Time left:" + timeLeft); // this adds a label (pop up) that shows how much time is left(updates every second)
 	    timerLabel.setBounds(10,10,100,30); // positions the label in the screen
 	    this.add(timerLabel);
-	    
+         
+		distance = 0;
+		distanceLabel = new JLabel ("Distance covered: " + distance + "m");// new label that shows the distance player moves
+	    distanceLabel.setBounds(120,10,100,30);
+		this.add(distanceLabel);
+
+
         clickButton = new JButton("click me"); // adds the button to be clicked to move player
         clickButton.setBounds(250,10,100,30);// sets position of clickbutton.
         clickButton.addActionListener(e - > handleClick()); // the action listener will handle each click.
-
+        
+		Random rand = new Random(); // this the isntance of teh random class
 	    // intiliaize the NPCs
 		for( int i = 0 ; i < 3; i ++){ // we are going to be adding 3 NPC to the game
 			int startY = 100 + i* 50; // this will posiiton the npcs along differnet points of the vertical axis
-			int speed = 5 + random.nextInt(6); // randomizes speed of NPCs so eahc will have a differnet speed
+			int speed = 5 + rand.nextInt(6); // randomizes speed of NPCs so eahc will have a differnet speed
 			npcs.add(new NPC(10,startY,speed));//each npcs as same starting X positon but different everything else
 		}
-	
-	
+
+        gameTimer = new Timer(timer_delay,this);// creates the timer and we use this to refer the current instance of the class
+	    gameTimer.start(); // action listener will handle the timers events.
+		
+	private void handleClick() {// this method is private as it is not relevent for external use.
+		player.move( );
+		distance = player.getX() * 0.01; // each pixel represnts 0.01 metres
+		distanceLabel.setText("distance covered: " + distance + "m");
+		repaint(); // this will re do the "screen" of the game so that the sprites can move
+	}	
+	@Override // method is intended to override a method in its super class however if it doesnt the compiler will generate a mistake
+	protected void paintComponent(Graphics g) { // this method will paint the games "screen" its protected so subclasses can inherit and pottentially override methods.
+		super.paintComponent(g);
+		g.drawImage(background, backgroundX, 0 , this); //draws first part of background
+		g.drawImage(background,backgroundX + background.getWidth(), 0 , this)// loops background to give scorlling affect
+		g.setFont(new Font("Arial", Font.BOLD, 40)); // the font ensure our sprite is desired style and size
+		g.drawString("🏃", player.getX(), player.getY()); // i will most likely create an actual sprite for the player with running animation but for now im keeping it an emoji.
+		for(NPC npc : npcs) {
+			g.drawString()
+		}
+	}
+		
+
 	
 }
